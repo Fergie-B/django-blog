7481e049258125cb90a78f03077ba516a30f18ec
Barry Ferguson <barry@revolvecreative.ie>
2023-05-29 11:19:57 +0000

    Added Comment functionality

--- codestar/settings.py
+++ codestar/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'django_summernote',
+    'crispy_forms',
     'blog',
     'cloudinary',
 ]
@@ -60,6 +61,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
